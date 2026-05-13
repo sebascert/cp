88e7c65a40f9e4bdceac84e235c0361a3601d9a5
Sebastian Certuche <sebascertuche@gmail.com>
2026-05-13 10:47:48 -0600

    Add 2D segtree

---
+++ lib/data_structure/2d_segtree.cpp
@@ -0,0 +1,42 @@
+#include "template.hpp"
+
+const int N = 0;
+int n, m;
+lli a[N][N];
+
+#define op(a, b) (a + b)
+const lli NEUT = 0;
+lli st[2 * N][2 * N];
+void build() {
+	fore(i, 0, n) fore(j, 0, m) st[i + n][j + m] = a[i][j];
+	fore(i, 0, n) ford(j, m, 1) st[i + n][j] =
+	    op(st[i + n][j << 1], st[i + n][j << 1 | 1]);
+	ford(i, n, 1) fore(j, 0, 2 * m) st[i][j] =
+	    op(st[i << 1][j], st[i << 1 | 1][j]);
+}
+void upd(int x, int y, lli v) {
+	st[x + n][y + m] = v;
+	for (int j = y + m; j > 1; j >>= 1)
+		st[x + n][j >> 1] = op(st[x + n][j], st[x + n][j ^ 1]);
+	for (int i = x + n; i > 1; i >>= 1)
+		for (int j = y + m; j; j >>= 1)
+			st[i >> 1][j] = op(st[i][j], st[i ^ 1][j]);
+}
+lli query(int x0, int x1, int y0, int y1) { //[x0,x1) [y0,y1)
+	lli r = NEUT;
+	for (int i0 = x0 + n, i1 = x1 + n; i0 < i1; i0 >>= 1, i1 >>= 1) {
+		int t[4], q = 0;
+		if (i0 & 1)
+			t[q++] = i0++;
+		if (i1 & 1)
+			t[q++] = --i1;
+		fore(k, 0, q) for (int j0 = y0 + m, j1 = y1 + m; j0 < j1;
+				   j0 >>= 1, j1 >>= 1) {
+			if (j0 & 1)
+				r = op(r, st[t[k]][j0++]);
+			if (j1 & 1)
+				r = op(r, st[t[k]][--j1]);
+		}
+	}
+	return r;
+}
